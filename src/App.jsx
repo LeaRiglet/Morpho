f804575f860cd8fac3bcc5d9c9822b966c1bcf6b
Louis Paulet <louispaulet13@gmail.com>
2025-06-22 14:26:57 +0200

    add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import DevenirImprimeur from './pages/DevenirImprimeur'
 import Products from './pages/Products'
+import Contact from './pages/Contact'
 import './App.css'
 
 function App() {
@@ -13,13 +14,14 @@ function App() {
           <Link to="/">Accueil</Link>
           <Link to="/products">Produits</Link>
           <Link to="/devenir-imprimeur">Imprimeur ?</Link>
-          <a href="#">Contact</a>
+          <Link to="/contact">Contact</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/devenir-imprimeur" element={<DevenirImprimeur />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   )
